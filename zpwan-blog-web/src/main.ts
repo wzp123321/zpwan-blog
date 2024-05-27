bee8d2dfaddbe72acb16fa0a37eb4000d22adb2a
wzp123321 <wanzhipengx@163.com>
2024-05-27 23:01:11 +0800

    feat(main)：安装组件库，删除无用文件

--- zpwan-blog-web/src/main.ts
+++ zpwan-blog-web/src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './app.vue';
+// 组件库样式
+import 'element-plus/dist/index.css';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
